fa12ed22d0e04f4f6382d3898422dfd104ecd553
Surya Prakash <surya0prakash@proton.me>
2026-02-10 08:54:48 +0530

    feat: cloudflare pivot 🤧

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://suryaaprakassh.github.io',
-    base: 'blog',
-    integrations: [mdx(), sitemap()],
+  site: 'http://suryaprakash.vip',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
